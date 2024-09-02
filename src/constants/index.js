00ace690bfee171d95735ca041ade621e238bae3
Rashmi Jadhav <jrashmi853@gmail.com>
2024-09-02 17:50:34 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project1.png";
+import project2 from "../assets/projects/project2.png";
+import project3 from "../assets/projects/project3.png";
+import project4 from "../assets/projects/project4.png";
+import project5 from "../assets/projects/project5.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Aug 2023 - Mar 2024",
+    role: "MERN Stack Developer Intern",
+    company: "LiveWire",
+    description: `Mastered advanced web technologies and industry practices through the development of sophisticated web applications using the MERN stack.  Worked on real-world projects, enhancing understanding of database management, API integration, and responsive design. Led the creation and deployment of dynamic, high-performance web projects, significantly improving user experience and functionality.`,
+    technologies: ["HTML5", "CSS3", "Javascript", "React JS", "Node.js", "Express js", "MongoDB"],
+  },
+  {
+    year: "Jan 2024 - Feb 2024",
+    role: "Web Developer Intern",
+    company: "MotionCut",
+    description: `Gained hands-on experience by working on industry-based problems, enhancing practical knowledge in web development.`,
+    technologies: ["HTML5", "CSS3", "Javascript"],
+  }
+ 
+];
+
+export const PROJECTS = [
+  {
+    title: "Static UI",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS"],
+  },
+  {
+    title: "Digital Clock",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Calculator",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Tip Calculator",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Weather",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+];
+
+export const CONTACT = {
+  address: "63, Chitnis Nagar, Nagpur, India",
+  phoneNo: "+91 7887403491",
+  email: "jadhav.n.rashmi@gmail.com",
+};
